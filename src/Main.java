af3a1b6e15ef16d3155313c2037323d3ffd1c6a9
bomblock <ekwotmd@gmail.com>
2023-02-04 00:03:47 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import java.util.ArrayList;
+
+import static jdk.internal.org.objectweb.asm.tree.Util.add;
+
+public class Main {
+
+    public static void main(String[] args) {
+        add(50,10);
+
+    public static void add(int x, int y){
+        System.out.println(x+y);
+        }
+
+    }
+}
